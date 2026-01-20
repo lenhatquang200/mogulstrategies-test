20f61b7b156c5998a4c26820034fbd40bea19844
lenhatquang200 <kendev2021@gmail.com>
2026-01-20 09:59:45 +0700

    stagging

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+
 import { handlers } from "@/auth"; // import from src/auth.ts
 
 export const { GET, POST } = handlers;
+
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const adapter = new PrismaMariaDb({
+    host: 'turntable.proxy.rlwy.net',
+    port: 39752,
+    user: 'root',
+    password: 'vMznmgmoVcfWfvqNwJMSuTtfTvGbbZtV',
+    database: 'MogulStrategies',
+    connectTimeout: 30000,
+    // @ts-ignore - SSL support in adapter-mariadb
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        adapter,
+        log: ['error', 'warn'],
+    });
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
