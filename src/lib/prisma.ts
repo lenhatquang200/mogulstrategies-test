bbc73cd90584237654d60f7faf48996eaf2f96b9
lenhatquang200 <kendev2021@gmail.com>
2026-01-20 10:50:08 +0700

    u:

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,26 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
 const createPrismaClient = () => {
-    const adapter = new PrismaMariaDb({
-        host: 'turntable.proxy.rlwy.net',
-        port: 39752,
-        user: 'root',
-        password: 'vMznmgmoVcfWfvqNwJMSuTtfTvGbbZtV',
-        database: 'MogulStrategies',
-        connectTimeout: 30000,
-        // @ts-ignore
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
-
     return new PrismaClient({
-        adapter,
         log: ['error', 'warn'],
     });
 };
